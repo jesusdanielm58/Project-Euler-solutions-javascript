bdb5402dd9c06647feb6fd82b9c2fc7a80389851
jesusdanielm58 <33935377+jesusdanielm58@users.noreply.github.com>
2017-12-04 14:44:09 -0400

    change to a function

--- problem 2.js
+++ problem 2.js
@@ -1,13 +1,16 @@
-var total=0; a=0;b=1;c=0;
-while (true) {
-	c=b+a;
-	a=b;
-	b=c;
-	if (c>4000000) {
-		break
+function problem2(){
+	var total=0; a=0;b=1;c=0;
+	while (true) {
+		c=b+a;
+		a=b;
+		b=c;
+		if (c>4000000) {
+			break
+		}
+		else{
+			if (c%2===0){total+=c}
+		}	
 	}
-	else{
-		if (c%2===0){total+=c}
-	}	
+	return total
 }
-console.log(total);
+problem2();
