10ecbd55a5abf847d3bbde1e2725cd828fd077fe
jesusdanielm58 <33935377+jesusdanielm58@users.noreply.github.com>
2017-12-04 14:40:46 -0400

    change to a function

--- problem 1.js
+++ problem 1.js
@@ -1,3 +1,6 @@
-var total=0;
-for (var i=0;i<1000;i++){ if (i%3===0 || i%5===0) {total+=i}}
-console.log(total);
+function problem1(){
+  var total=0;
+  for (var i=0;i<1000;i++){ if (i%3===0 || i%5===0) {total+=i}}
+  return total
+}
+problem1();
