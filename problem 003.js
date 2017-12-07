c8573d69f92feb8858f69726269ca9a81a5d3ae0
jesusdanielm58 <33935377+jesusdanielm58@users.noreply.github.com>
2017-12-07 16:37:34 -0400

    problem description added

--- problem 003.js
+++ problem 003.js
@@ -1,3 +1,8 @@
+/*Largest prime factor
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
 function prime (primo){
 	var n=primo[primo.length-1]
 	while(true){
